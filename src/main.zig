570cefcc21f007e422c3015a914ff55899778982
Luxe17 <hanson1717@live.com>
2026-09-02 11:20:03 -0500

    Format code

--- src/main.zig
+++ src/main.zig
@@ -5,15 +5,15 @@ const repl = @import("repl.zig");
 pub fn main(init: std.process.Init) !void {
     const io = init.io;
 
-    var stdin_buffer: [1024]u8 = undefined;
     var stdout_buffer: [1024]u8 = undefined;
     var stdout_writer = std.Io.File.stdout().writer(io, &stdout_buffer);
-    var stdin_reader = std.Io.File.stdin().reader(io, &stdin_buffer);
-    const stdin = &stdin_reader.interface;
     const stdout = &stdout_writer.interface;
 
+    var stdin_buffer: [1024]u8 = undefined;
+    var stdin_reader = std.Io.File.stdin().reader(io, &stdin_buffer);
+    const stdin = &stdin_reader.interface;
+
     try stdout.writeAll("Hello! This is the Monkey programming language\n");
-    try stdout.flush();
     try stdout.writeAll("Feel free to type in commands\n");
     try stdout.flush();
 
